538c98e516197a8a32d8646fd33a363aa3719df5
Danny Friedman <dafrdman@gmail.com>
2022-03-07 17:04:07 -0500

    benchmark

--- model/model.py
+++ model/model.py
@@ -1,13 +1,20 @@
 import os
 import sys
-from collections import defaultdict
 from pathlib import Path
 import pandas as pd
 import numpy as np
+import importlib
 import ipdb
-from nba_api.stats.endpoints import playbyplayv2
-from nba_api.stats.endpoints import leaguegamefinder
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Dropout, Activation
+from sklearn.preprocessing import StandardScaler
+
 hoops_dir = Path(os.path.abspath(__file__)).parent.parent
 data_dir = hoops_dir / "data"
+model_dir = hoops_dir / "model"
 sys.path.append(hoops_dir.as_posix())
 
+def score_model():
+    os.makedirs(model_dir / "scores", exist_ok=True)
+    
